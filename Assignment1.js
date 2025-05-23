9f2dd8386e3a8b4aa879a6a5f7f5b0174bc1b325
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-23 18:28:07 +0530

    Add files via upload

--- Assignment1.js
+++ Assignment1.js
@@ -1,4 +1,5 @@
                     //JAVASCRIPT ASSIGNMENT-1
+                    
 //Q1. Add two Numbers
 //Answers-->
 let A = 2;  // declare the varaible a and assign value 2
@@ -22,9 +23,38 @@ console.log(check_div);
 
 //Q4.Find the first digit of a 4 digit number.
 //Answer--->
-let N = 4567;         // 4-digit number
-let mod = N % 1000;   // last 3 digits: 4567 % 1000 = 567
-let sub = N - mod;    // 4567 - 567 = 4000
-let firstDigit = sub / 1000;  // 4000 / 1000 = 4
+let N=4567;  // 4-digit number
+let mod=N%1000; // last 3 digits: 4567 % 1000 = 567
+let sub=N-mod; // 4567 - 567 = 4000
+let firstDigit=sub/1000;  // 4000 / 1000 = 4
+console.log(firstDigit);//Output: 4
 
-console.log(firstDigit);  // Output: 4
+//Q5.Find the last digit of a 4 digit number
+//Answer--->
+let N =4567; // 4-digit number
+let lastdigit =N % 10;
+console.log(lastdigit);//Output 7
+
+//Q6.Find the remainder
+//Answer--->
+let r1 = 2; // divisor
+let r2 = 9; // dividend
+console.log(r1%r2);//remainder is 1
+
+//Q7.Multiply two Numbers
+//Answer--->
+let m1 = 2;
+let m2 = 5;
+console.log(m1*m2);//Output 10
+
+//Q8. Marks Calculator 
+//Answer--->
+let mar1=50;
+let mar2=20;
+let mar3=100;
+let total = mar1+mar2+mar3;
+let average=total/3;
+console.log(total);//Print total marks of Shyam.
+console.log(average); //Print Average of Shyam.
+
+
